e358cfca2ed7c1e48d52c3197580d23ede31820f
seohyeon <uimi3606@gmail.com>
2024-05-14 17:45:21 +0900

    git init

---
+++ jwtTask/src/main/java/com/springboot/jwttask/JwtTaskApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jwttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTaskApplication.class, args);
+    }
+
+}
---
+++ jwtTask/src/main/java/com/springboot/jwttask/entity/User.java
@@ -0,0 +1,11 @@
+package com.springboot.jwttask.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@
+public class User {
+}
